f3418439fe35dfedc285b32ea3ceed68e0fc8779
melkie yilk <melkieyilk@gmail.com>
2025-03-13 18:31:21 +0300

    Add the main header components

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,6 +1,8 @@
 import React from "react";
+
 import "./UsersList.css";
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 
 const UsersList = (props) => {
   const { items } = props;
@@ -8,7 +10,9 @@ const UsersList = (props) => {
   if (items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
